2e7a13d397b8db9de755cb653fac2706564bde11
Bruno Santos <brunusuxx@gmail.com>
2026-08-19 19:35:11 -0300

    atualização service work para v9.6

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Cuida do cache dos arquivos estáticos para o app funcionar offline
 // e poder ser instalado no celular (PWA).
 
-const CACHE_NAME = 'finpessoal-cache-v9.5';
+const CACHE_NAME = 'finpessoal-cache-v9.6';
 
 const ASSETS_TO_CACHE = [
   './',
